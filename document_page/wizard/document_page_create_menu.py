e2f845331481a89079628c56b33708f44cc74f4a
jssuzanne <jssuzanne@anybox.fr>
2014-10-30 10:24:09 +0100

    Allow other user to create menu

--- document_page/wizard/document_page_create_menu.py
+++ document_page/wizard/document_page_create_menu.py
@@ -20,6 +20,8 @@
 ##############################################################################
 
 from openerp.osv import fields, osv
+from openerp import SUPERUSER_ID
+
 
 class document_page_create_menu(osv.osv_memory):
     """ Create Menu """
@@ -34,7 +36,7 @@ class document_page_create_menu(osv.osv_memory):
     def default_get(self, cr, uid, fields, context=None):
         if context is None:
             context = {}
-        res = super(document_page_create_menu,self).default_get(cr, uid, fields, context=context)
+        res = super(document_page_create_menu, self).default_get(cr, uid, fields, context=context)
         page_id = context.get('active_id')
         obj_page = self.pool.get('document.page')
         page = obj_page.browse(cr, uid, page_id, context=context)
@@ -45,7 +47,6 @@ class document_page_create_menu(osv.osv_memory):
         if context is None:
             context = {}
         obj_page = self.pool.get('document.page')
-        obj_view = self.pool.get('ir.ui.view')
         obj_menu = self.pool.get('ir.ui.menu')
         obj_action = self.pool.get('ir.actions.act_window')
         page_id = context.get('active_id', False)
@@ -69,18 +70,14 @@ class document_page_create_menu(osv.osv_memory):
         value['domain'] = "[('parent_id','=',%d)]" % (page.id)
         value['res_id'] = page.id
 
-        action_id = obj_action.create(cr, uid, value)
-        menu_id = obj_menu.create(cr, uid, {
-                        'name': data.menu_name,
-                        'parent_id':data.menu_parent_id.id,
-                        'icon': 'STOCK_DIALOG_QUESTION',
-                        'action': 'ir.actions.act_window,'+ str(action_id),
-                        }, context)
-        obj_page.write(cr, uid, [page_id], {'menu_id':menu_id})
+        action_id = obj_action.create(cr, SUPERUSER_ID, value)
+        menu_id = obj_menu.create(cr, SUPERUSER_ID, {
+            'name': data.menu_name,
+            'parent_id': data.menu_parent_id.id,
+            'icon': 'STOCK_DIALOG_QUESTION',
+            'action': 'ir.actions.act_window,' + str(action_id)}, context)
+        obj_page.write(cr, SUPERUSER_ID, [page_id], {'menu_id': menu_id})
         return {
             'type': 'ir.actions.client',
             'tag': 'reload',
         }
-
-
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
